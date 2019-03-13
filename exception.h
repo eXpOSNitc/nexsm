0806fa7bb196fed97b39d76d65fd41ad2a720860
Arun Joseph <arunjosephkv2@gmail.com>
2019-03-13 13:58:42 +0530

    Completed disk and exception

--- exception.h
+++ exception.h
@@ -18,4 +18,12 @@ typedef struct _xsm_exception
     int epn;
 } xsm_exception;
 
+int exception_set(char *message, int type, int mode);
+char *exception_message();
+int exception_code();
+void exception_set_ma(int address);
+void exception_set_epn(int page);
+int exception_get_ma();
+int exception_get_epn();
+
 #endif
